4abd1277f56a2537086c6c103d4c241753d5f31f
Brian Fang <fangbrian@gmail.com>
2017-10-26 16:29:14 -0700

    Basic web ui structure

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,9 +5,16 @@ function fetchGame() {
 	    url: '/game'
 	}).done(function (data) {
 	    console.log(data);
-	    $('#option1').text(data.result.options[0].num_of_votes);
-	    $('#option2').text(data.result.options[1].num_of_votes);
-	    $('#option3').text(data.result.options[2].num_of_votes);
+	    $('#option1Votes').text(data.result.options[0].num_of_votes);
+	    $('#option2Votes').text(data.result.options[1].num_of_votes);
+	    $('#option3Votes').text(data.result.options[2].num_of_votes);
+
+	    $('#option1ProfilePicture').attr("src", data.result.options[0].img_url);
+	    $('#option2ProfilePicture').attr("src", data.result.options[1].img_url);
+	    $('#option3ProfilePicture').attr("src", data.result.options[2].img_url);
+
+		$("#profilePicture").attr("src", data.result.profile[0].img_url);
+	    
 	    
 	}).fail(function (e) { 
 	    
@@ -18,6 +25,6 @@ $(document).ready(function() {
 	fetchGame();
 
 	var interval = setInterval(function() {
-        fetchGame();
+        // fetchGame();
     }, 1000)
 });
