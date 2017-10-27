abe191c90c50965cdeff4dc21c84adfc652f4686
Brian Fang <fangbrian@gmail.com>
2017-10-26 19:50:02 -0700

    Add white border

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -14,33 +14,42 @@ function fetchGame() {
 	    $('#option3ProfilePicture').attr("src", data.result.options[2].img_url);
 
 		$("#profilePicture").attr("src", data.result.profile[0].img_url);
+
+		$('#option1ProfilePicture').css({opacity: 1});
+		$('#option2ProfilePicture').css({opacity: 1});
+		$('#option3ProfilePicture').css({opacity: 1});
 	    
 		$("#headerMessage").text("Who is the best match?");
+
 	    if (data.result.is_complete) {
 	    	var option1Votes = data.result.options[0].num_of_votes;
 		    var option2Votes = data.result.options[1].num_of_votes;
 		    var option3Votes = data.result.options[2].num_of_votes;
 		    var highestNumberOfVotes = 0;
 
-		    if (option1Votes >= highestNumberOfVotes) {
+		    if (option1Votes > highestNumberOfVotes) {
 		    	highestNumberOfVotes = option1Votes;
 		    } else {
-		    	$('#option1ProfilePicture').attr("opacity", 0.5);
+		    	$('#option1ProfilePicture').css({opacity: 0.5});
 		    }
 
-		    if (option2Votes >= highestNumberOfVotes) {
+		    if (option2Votes > highestNumberOfVotes) {
 		    	highestNumberOfVotes = option2Votes;
 		    } else {
-		    	$('#option2ProfilePicture').attr("opacity", 0.5);
+		    	$('#option2ProfilePicture').css({opacity: 0.5});
 		    }
 
-		    if (option3Votes >= highestNumberOfVotes) {
+		    if (option3Votes > highestNumberOfVotes) {
 		    	highestNumberOfVotes = option3Votes;
 		    } else {
-		    	$('#option3ProfilePicture').attr("opacity", 0.5);
+		    	$('#option3ProfilePicture').css({opacity: 0.5});
 		    }
 
-		    $("#headerMessage").text("We have a winner");
+		    if (highestNumberOfVotes == 0) {
+		    	$("#headerMessage").text("Game Over. \n Nothing decided");
+		    } else {
+		    	$("#headerMessage").text("We have a winner. Sending bagel now...");
+			}
 	    }
 	    
 	}).fail(function (e) { 
